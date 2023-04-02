bf67b487992d97c104789f0de94e228bf377b72c
Evan Williams <evskimo97@gmail.com>
2023-04-02 20:00:56 +0100

    Editor Tool complete

--- EvMeshPro/Assets/Editor/StyleSheetCustomEditor.cs
+++ EvMeshPro/Assets/Editor/StyleSheetCustomEditor.cs
@@ -1,26 +1,32 @@
 using System;
-using System.Management.Instrumentation;
-
-using Codice.CM.SEIDInfo;
-
+using System.Collections.Generic;
 using TMPro;
-
-using Unity.Mathematics;
-
 using UnityEngine;
 using UnityEditor;
 
 [CustomEditor(typeof(SO_TextStyleList))]
 public class StyleSheetCustomEditor : Editor
 {
-
-   private GameObject previewGameObject;
+   //Custom Editor script for previewing changes to custom style tags
+   private GameObject previewGameObject; //References the currently instantiated preview canvas
+   private int selected = 0; //Keeps track of the selected ID index
    
    public override void OnInspectorGUI() {
+      SO_TextStyleList previewList = (SO_TextStyleList)target;
+      
+      //Create a popup of ID's on the current custom style list
+      List<string> popupOptions = new List<string>();
+      foreach (CustomTextStyle style in previewList.customTextStyles) {
+         popupOptions.Add(style.tagID);
+      }
+      string[] options = popupOptions.ToArray();
+      selected = EditorGUILayout.Popup("Label", selected, options); 
+      
       
       if (GUILayout.Button("Preview In Scene")) {
-        PreviewInScene();
+        PreviewInScene(options[selected]);
       }
+      
       base.OnInspectorGUI();
    }
 
@@ -30,21 +36,26 @@ public class StyleSheetCustomEditor : Editor
       }
    }
 
-   public void PreviewInScene() {
+   public void PreviewInScene(string id) {
       SO_TextStyleList previewList = (SO_TextStyleList)target;
 
+      //Destroy our current preview object in the scene (cleaner than replacing shit)
       if (previewGameObject) { DestroyImmediate(previewGameObject); }
 
+      //Instantiate our preview canvas from resources and locate its text asset
       GameObject previewObj = Resources.Load<GameObject>("Canvas_PreviewExample");
-      previewGameObject = Instantiate(previewObj, Vector3.zero, quaternion.identity);
+      previewGameObject = Instantiate(previewObj, Vector3.zero, Quaternion.identity);
       TMP_Text textAsset = previewGameObject.GetComponentInChildren<TMP_Text>();
 
-      string textToPreview = "[TEST]" + previewList.previewSampleText + "[/TEST]";
+      //Create the preview text and add our ID tags to both ends
+      string textToPreview = "[" + id + "]" + previewList.previewSampleText + "[/" + id + "]";
 
+      //Create a dialogue controller on our preview canvas and use that to generate our rich text string
       DialogueController previewDialogueController = previewGameObject.AddComponent<DialogueController>();
       previewDialogueController.textStyleList = previewList;
       string parsedText = previewDialogueController.ParseDialogueCustomStyle(textToPreview);
 
+      //Assign our new rich text to the text asset on our preview canvas
       textAsset.text = parsedText;
    }
 }
